e2a0f0a4cfc38446926d3221620cda3440942552
vivekjuremani <93486394+vivekjuremani@users.noreply.github.com>
2023-04-08 19:14:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,12 +12,17 @@ import authenticateUser from './middleware/auth.js';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
 app.use(express.json());
-
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 app.use(express.static(path.join(__dirname, './frontend/build')));
 app.use('/api/v1/auth', authRouter);
 app.use(notFound);
